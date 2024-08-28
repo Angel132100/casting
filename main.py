664e3926b47604e3e3c885754f91019f7a4ac6e9
Angel Parker <parkeang000@warren.k12.in.us>
2024-08-28 10:14:36 -0400

    First commit

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ print("The temp in degrees C is", C)
 
 C_text = input("Enter the temp in degrees C: ")
 C = float(C_text)
-F = 5 * (C - 32) / 9
-print("The temp in C is" , F)
+F = 1.8 * C + 32
+print("The temp in F is", F)
